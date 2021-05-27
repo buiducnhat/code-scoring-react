03cc7f32abba6233dd317827cc499a4a01b59c4f
gerpan <nhaths4701gmail.com>
2021-05-27 10:04:39 +0700

    fix

--- public/electron.js
+++ public/electron.js
@@ -10,7 +10,7 @@ const url = require('url');
 let mainWindow;
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow = new BrowserWindow({ width: 1366, height: 768 });
   // and load the index.html of the app.
   const startUrl =
     process.env.ELECTRON_START_URL ||
